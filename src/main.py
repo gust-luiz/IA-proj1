06516bf8b33044576f96e00735549f49fb71a157
rodrigofegui <rodrigofegui@gmail.com>
2020-09-17 11:26:20 -0300

    Add: Prints to gather evolution data

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-from genetic_algorithm import (_crossover_2_point,crossover, fitness, get_initial_generation,
+import variables
+from genetic_algorithm import (crossover, fitness, get_initial_generation,
                                get_next_generation, get_total_distance,
                                mutation)
 from utils import get_named_route, reached_stability
@@ -6,17 +7,21 @@ from variables import GENERATION_MAX
 
 generation = get_initial_generation()
 
+print('#gen\tmin\tavg')
+
 for cnt in range(GENERATION_MAX):
     generation = fitness(generation)
 
+    print(f'{cnt}\t{get_total_distance(generation[0])}\t{variables.avg_distance}')
+
     if reached_stability(generation):
-        print(f'stabilized at {cnt}')
         break
 
     new_individuals = crossover(generation)
     new_individuals.extend(mutation(generation))
 
-    generation = get_next_generation(generation, new_individuals)
+    if cnt != GENERATION_MAX - 1:
+        generation = get_next_generation(generation, new_individuals)
 
 print('best route:')
 print(get_named_route(generation[0]))
